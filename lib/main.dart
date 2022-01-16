82a24abbe5d08ccd7def6085972e322cb08c7225
710660Thanakrit <95558256+710660Thanakrit@users.noreply.github.com>
2022-01-16 12:14:12 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,76 +13,149 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.pink,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  final _controllerL = TextEditingController();
+  final _controllerG = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Container(
         constraints: BoxConstraints.expand(),
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('images/bg.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-
-        //alignment: Alignment.center,
         child: Center(
           child: Column(
-            //mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
-
+            mainAxisSize: MainAxisSize.min,
+            //crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                'PIG WEIGHT',
-                style: TextStyle(
-                    fontSize: 40.0, color: Colors.pink.shade500),
-              ),
-              Text(
-                'CALCULATOR',
-                style: TextStyle(
-                    fontSize: 40.0, color: Colors.pink.shade500),
-              ),
-
-              Image.asset('images/pig.png', width: 280.0,),
-
               Row(
+                //mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  Text(
+                    'PIG WEIGHT\n Calculator',
+                    style: TextStyle(
+                      fontSize: 30.0, color: Colors.pink.shade500,),
+                    textAlign: TextAlign.center,
 
-                  Column(
-                    children: [
-
-                    ],
                   ),
                 ],
               ),
-
-              TextField(),
-              SizedBox(
-                height: 20,
+              SizedBox(height: 20.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('images/pig.png', width: 300.0),
+                ],
               ),
-              ElevatedButton(
-                child: Text('CALCULATE'),
-                onPressed: () {},
+              Expanded( child: Container()),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      controller: _controllerL,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.7),
+                        border: OutlineInputBorder(),
+                        labelText: 'LENGTH\n(cm)',
+                        hintText: 'Enter Length',
+                      ),
+                    ),
+
+                  ),
+                  Expanded(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      controller: _controllerG,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.7),
+                        border: OutlineInputBorder(),
+                        labelText: 'GIRTH\n(cm)',
+                        hintText: 'Enter girth',
+                      ),
+                    ),
+
+                  ),
+
+                ],
+              ),
+              Expanded(
+                child: Center(
+                  child: ElevatedButton(
+                      child: Text('Calculate'),
+                      onPressed: () {
+                        var length = _controllerL.text;
+                        var lengthD = double.tryParse(length);
+                        var lengthR = lengthD! / 100.0;
+                        var girth = _controllerG.text;
+                        var girthD = double.tryParse(girth);
+                        var girthR = girthD! / 100.0;
+                        if(lengthD != null && girthD != null) {
+                          var weight = lengthR * girthR * girthR * 69.3;
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                    title: Text('Result!'),
+                                    content: Text('Weight: ${(weight-weight*0.3).round()} - ${(weight+weight*0.3).round()}\n'
+                                        'Price: ${((weight-weight*0.3)*112.50).round()} - ${((weight+weight*0.3)*112.50).round()}'),
+                                    actions: [
+                                      ElevatedButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          }, child: Text('OK'))
+                                    ]
+                                );
+                              });
+                        }
+                        else if(lengthD == null || girthD == null){
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Error!'),
+                                  content: Text('Invalid Input!!!'),
+                                  actions: [
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        }, child: Text('OK'))
+                                  ],
+                                );
+                              });
+                        }
+                      }
+                  ),
+                ),
               ),
             ],
           ),
-
         ),
-
       ),
     );
   }
